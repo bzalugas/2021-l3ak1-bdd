1e51b69c1236183868c0f4bac5047fd0da6cd292
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2022-04-04 01:06:00 +0200

    Change database connection method

--- config/Database.php
+++ config/Database.php
@@ -2,32 +2,51 @@
 
 class Database
 {
-	private $dotenv;
+	// private $dotenv;
 	private $dbConn = null;
 
 	public function __construct()
 	{
-		$this->dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-		$this->dotenv->load();
-		$host = $_ENV['DB_HOST'];
-		$port = $_ENV['DB_PORT'];
-		$db = $_ENV['DB_DATABASE'];
-		$user = $_ENV['DB_USERNAME'];
-		$pass = $_ENV['DB_PASSWORD'];
+		// $this->dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+		// $this->dotenv->load();
+		// $host = $_ENV['DB_HOST'];
+		// $port = $_ENV['DB_PORT'];
+		// $db = $_ENV['DB_DATABASE'];
+		// $user = $_ENV['DB_USERNAME'];
+		// $pass = $_ENV['DB_PASSWORD'];
+
+		$db = parse_url(getenv("DATABASE_URL"));
 
 		try
 		{
-			$this->dbConn = new PDO(
-				"pgsql:host=$host;port=$port;charset=utf8;dbname=$db",
-				$user,
-				$pass,
-				[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+			$this->dbConn = new PDO("pgsql:" . sprintf(
+				"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+				$db["host"],
+				$db["port"],
+				$db["user"],
+				$db["pass"],
+				ltrim($db["path"], "/")
+				)
 			);
 		}
 		catch (Exception $e)
 		{
 			die('Error in' . __CLASS__ . ' : '.$e->getMessage());
 		}
+
+		// try
+		// {
+		// 	$this->dbConn = new PDO(
+		// 		"pgsql:host=$host;port=$port;charset=utf8;dbname=$db",
+		// 		$user,
+		// 		$pass,
+		// 		[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+		// 	);
+		// }
+		// catch (Exception $e)
+		// {
+		// 	die('Error in' . __CLASS__ . ' : '.$e->getMessage());
+		// }
 	}
 
 	public function getConnection()
